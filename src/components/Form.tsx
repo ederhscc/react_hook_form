181be186a2a57c85bf6826c4450647544d8baa91
Eder Henrique <ederhscc@gmail.com>
2024-07-14 11:21:47 -0300

    added first validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,28 @@
-const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+import { useForms } from "../hooks/useForms";
+import { FormSchema } from "../schemas/formSchema";
 
-export default Form
+const Form = () => {
+  const { register, handleSubmit, errors } = useForms();
+
+  const onSubmit = (data: FormSchema) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          id="name"
+          placeholder="Digite o seu nome"
+          {...register("name")}
+        />
+        {errors.name && <small>{errors.name.message}</small>}
+      </div>
+      <button type="submit">Cadastrar</button>
+    </form>
+  );
+};
+
+export default Form;
